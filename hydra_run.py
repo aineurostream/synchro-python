0995966658fb3d627ab742cd77616a58a95440aa
Stanislav Gonorovskii <drmozg@gmail.com>
2024-12-21 17:07:31 +0400

    [CORE] Hydra sample configs

--- hydra_run.py
+++ hydra_run.py
@@ -4,18 +4,25 @@ from omegaconf import DictConfig, OmegaConf
 import hydra
 
 from synchro.config.schemas import ProcessingGraphConfig
+from synchro.config.settings import SettingsSchema
 from synchro.core import CoreManager
 
 
-@hydra.main(version_base=None)
+@hydra.main(version_base=None, config_path="config", config_name="config")
 def hydra_app(cfg: DictConfig) -> None:
     print(OmegaConf.to_yaml(cfg))
     """Start an instance of the Synchro application"""
 
-    pipeline_config = cast(dict[str, Any], cfg["pipeline_config"])
-    neural_config = cast(dict[str, Any], cfg["neural_config"])
+    pipeline_config = cast(DictConfig, cfg["pipeline"])
+    neural_config = cast(DictConfig, cfg["ai"])
+    settings_config = cast(DictConfig, cfg["settings"])
 
     core_config = ProcessingGraphConfig.model_validate(pipeline_config)
+    settings = SettingsSchema.model_validate(settings_config)
+    neural_config_dict = OmegaConf.to_container(neural_config)
 
-    core = CoreManager(core_config, neural_config)
+    core = CoreManager(core_config, neural_config_dict, settings)
     core.run()
+
+if __name__ == "__main__":
+    hydra_app()
--- synchro/core.py
+++ synchro/core.py
@@ -2,6 +2,7 @@ import logging
 from typing import Any
 
 from synchro.config.schemas import ProcessingGraphConfig
+from synchro.config.settings import SettingsSchema
 from synchro.graph.graph_initializer import GraphInitializer
 from synchro.graph.graph_manager import GraphManager
 
@@ -13,9 +14,11 @@ class CoreManager:
         self,
         pipeline_config: ProcessingGraphConfig,
         neuro_config: dict[str, Any],
+        settings: SettingsSchema,
     ) -> None:
         self._pipeline_config = pipeline_config
         self._neuro_config = neuro_config
+        self._settings = settings
 
     def run(self) -> None:
         logger.info("Starting Synchro instance")
@@ -24,7 +27,7 @@ class CoreManager:
             self._pipeline_config,
             self._neuro_config,
         ).build()
-        full_graph = GraphManager(nodes, edges)
+        full_graph = GraphManager(nodes, edges, self._settings)
         full_graph.execute()
 
         logger.info("Stopping Synchro instance")
