ceb07e242e1d5438c09c15be723144c54b40f9c8
Sergey Trofimov <truetug@gmail.com>
2025-03-06 16:37:01 +0300

    Fix logging ascii

--- synchro/logging.py
+++ synchro/logging.py
@@ -1,4 +1,11 @@
 import logging
+from pythonjsonlogger.json import JsonFormatter
+
+
+class CustomJsonFormatter(JsonFormatter):
+    def __init__(self, *args, **kwargs):
+        kwargs["json_ensure_ascii"] = False
+        super().__init__(*args, **kwargs)
 
 
 def setup_logging() -> None:
