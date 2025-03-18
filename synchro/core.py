9a88e55f95135a0426d9555b5ba6d7f25659f8be
Sergey Trofimov <truetug@gmail.com>
2025-03-18 13:10:27 +0300

    Fix always use utf-8 for logs

--- synchro/core.py
+++ synchro/core.py
@@ -28,8 +28,8 @@ class CoreManager:
     def preprocess_neuro_config(self) -> None:
         # Preprocess neuro config
         def load_from_file(file_path: str) -> str:
-            with open(file_path) as file:
-                return file.read()
+            with open(file_path, "r") as fp:
+                return fp.read()
 
         translate_map = self._neuro_config["translate"]
         swappable_keys = [
