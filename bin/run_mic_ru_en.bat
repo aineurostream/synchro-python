74db1ac6530b879f72801bf6c9a0dde12d210272
Sergey Trofimov <truetug@gmail.com>
2025-09-11 18:39:15 +0300

    Добавляет файлы для простого запуска на клиентских машинах

---
+++ bin/run_mic_ru_en.bat
@@ -0,0 +1,13 @@
+set SERVER="http://127.0.0.1:50080"
+
+// uv run python -m sounddevice
+set DEVICE_IN=0
+set DEVICE_OUT=5
+
+uv run python hydra_run.py ^
+    --config-name config ^
+    pipeline=mic_ru_en ^
+    settings.limits.run_time_seconds=0 ^
+    pipeline.nodes.0.device=%DEVICE_IN% ^
+    pipeline.nodes.6.device=%DEVICE_OUT% ^
+    pipeline.nodes.3.server_url=%SERVER%
---
+++ bin/run_mic_ru_en.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+SERVER="http://127.0.0.1:50080" \
+DEVICE_IN=0 \
+DEVICE_OUT=5 \
+uv run python hydra_run.py \
+    --config-name config \
+    pipeline=mic_ru_en \
+    settings.limits.run_time_seconds=0 \
+    pipeline.nodes.0.device=$DEVICE_IN \
+    pipeline.nodes.6.device=$DEVICE_OUT \
+    pipeline.nodes.3.server_url=$SERVER
